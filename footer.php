117ac49a8e26992ca485b3697858a591d392b2ae
stooges152 <51711582+stooges152@users.noreply.github.com>
2019-09-26 14:41:25 -0400

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,19 @@
+<html>
+
+<body>
+<div style="position:absolute; right:0; bottom:0;">
+<?php
+/*This is to output the copyright symball*/
+    function displayCopyRightInfo()
+    {
+        echo "&copy;2019";
+    }
+
+DisplayCopyRightInfo()
+?> 
+<a href="http://validator.w3.org/check?uri=referer"> <img src="http://www.w3.org/Icons/valid-xhtml10" alt="Valid XHTML 1.0 Strict" /> </a>
+</div>
+
+</body>
+
+</html>
